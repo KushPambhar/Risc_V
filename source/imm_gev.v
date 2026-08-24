de60674b04b48f79aa4c40947825766425aff2d4
Siddhi Garg <83530718+siddhigarg@users.noreply.github.com>
2026-08-24 18:09:01 +0530

    Update imm_gev.v

--- source/imm_gev.v
+++ source/imm_gev.v
@@ -8,7 +8,7 @@ module immgen(instruction,offset);
             7'b0000011 : offset ={{20{instruction[31]}},instruction[31:20]}; // I type (Load Type instr.)
             7'b0010011 : offset ={{20{instruction[31]}},instruction[31:20]}; // I type (Immediate Type instr.)
             7'b0100011 : offset = {{20{instruction[31]}}, instruction[31:25], instruction[11:7]}; // S Type
-            7'b1100111 : offset = {{19{instruction[31]}}, instruction[31], instruction[7], instruction[30:25], instruction[11:8], 1'b0}; // SB Type
+            7'b1100111 : offset = {{19{instruction[31]}}, instruction[31], instruction[7], instruction[30:25], instruction[11:8]}; // SB Type
             default : offset = 32'b0; 
         endcase
     end
