e8fe474c73036ecc2befb9dabeb87301767a4617
KushPambhar <97092048+KushPambhar@users.noreply.github.com>
2026-08-24 17:04:48 +0530

    Update instruction_memory.v

--- source/instruction_memory.v
+++ source/instruction_memory.v
@@ -5,7 +5,10 @@ module inst_mem(address, instruction_out);
     
     initial $readmemh("imem.hex",mem);
     
-    assign instruction_out=mem[address[31:0]];
+    assign instruction_out[7:0]=mem[address[9:0]];
+    assign instruction_out[15:8]=mem[address[9:0]+1];
+    assign instruction_out[23:16]=mem[address[9:0]+2];
+    assign instruction_out[31:24]=mem[address[9:0]+3];
 
 
 endmodule
