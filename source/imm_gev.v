ed50bd2dbbd7c9b32637e7d1831582392081f28c
KushPambhar <97092048+KushPambhar@users.noreply.github.com>
2026-08-24 17:29:35 +0530

    Update imm_gev.v

--- source/imm_gev.v
+++ source/imm_gev.v
@@ -5,9 +5,10 @@ module immgen(instruction,offset);
 
     always@(*) begin 
         case (opcode)
-            7'b0000011 : offset ={{20{instruction[31]}},instruction[31:20]};
-            7'b0100011 : offset = {{20{instruction[31]}}, instruction[31:25], instruction[11:7]};
-            7'b1100011 : offset = {{19{instruction[31]}}, instruction[31], instruction[7], instruction[30:25], instruction[11:8], 1'b0};
+            7'b0000011 : offset ={{20{instruction[31]}},instruction[31:20]}; // I type (Load Type instr.)
+            7'b0010011 : offset ={{20{instruction[31]}},instruction[31:20]}; // I type (Immediate Type instr.)
+            7'b0100011 : offset = {{20{instruction[31]}}, instruction[31:25], instruction[11:7]}; // S Type
+            7'b1100111 : offset = {{19{instruction[31]}}, instruction[31], instruction[7], instruction[30:25], instruction[11:8], 1'b0}; // SB Type
             default : offset = 32'b0; 
         endcase
     end
