130d6c8298cf412334257145ab2c6b149adca8e8
KushPambhar <97092048+KushPambhar@users.noreply.github.com>
2026-08-24 22:39:04 +0530

    Update Data_memory.v

--- source/Data_memory.v
+++ source/Data_memory.v
@@ -4,16 +4,26 @@ module data_mod(data_address,clk,write_data, Read_data, MemRead, MemWrite);
     output reg [31:0] Read_data;
 
 
-    reg [31:0] mem [0:63];
+    reg [7:0] mem [0:1023];
     initial $readmemh("dmem.hex", mem);
+    wire [9:0]add;
+    assign wire = data_address[9:0];
     always@(*) begin 
         Read_data = 32'b0;
-        if(MemRead)
-            Read_data = mem[data_address];
+        if(MemRead) begin
+            Read_data[7:0] = mem[add];
+            Read_data[15:8] = mem[add+1];
+            Read_data[23:16] = mem[add+2];
+            Read_data[31:24] = mem[add+3];
+        end
     end
 
     always@(posedge clk) begin 
-        if(MemWrite)
-            mem[data_address] <= write_data;
+        if(MemWrite) begin
+            mem[add] <= write_data[7:0];
+            mem[add+1] <= write_data[15:8];
+            mem[add+2] <= write_data[23:16];
+            mem[add+3] <= write_data[31:24];
+        end
     end
 endmodule
