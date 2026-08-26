6920545a6103da6abc6aa69d8a4c87e1edb9315e
KushPambhar <97092048+KushPambhar@users.noreply.github.com>
2026-08-26 14:51:17 +0530

    Update imm_gev.v

--- source/imm_gev.v
+++ source/imm_gev.v
@@ -11,8 +11,8 @@ module immgen(instruction,offset);
             7'b1100011 : offset = {{19{instruction[31]}}, instruction[31], instruction[7], instruction[30:25], instruction[11:8],1'b0}; // B Type
             7'b0110111 : offset = {instruction[31:12], {12{1'b0}}}; // Utype (LUI)
             7'b0010111 : offset = {instruction[31:12], {12{1'b0}}}; // Utype (AUIPC)
-            7'b1101111 : offset = {{11{instruction[31]}}, instruction[31], instruction[19:12], instruction[20], instruction[30:21],1'b0}; // J Type (JAL)
-            7'b1100111 : offset ={{20{instruction[31]}},instruction[31:20]}; // IJALR
+            // 7'b1101111 : offset = {{11{instruction[31]}}, instruction[31], instruction[19:12], instruction[20], instruction[30:21],1'b0}; // J Type (JAL)
+            // 7'b1100111 : offset ={{20{instruction[31]}},instruction[31:20]}; // IJALR
             default : offset = 32'b0; 
         endcase
     end
