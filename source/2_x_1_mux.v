8b1f16e8b30eae4ea445b74c1ba48e042a6c547b
Siddhi Garg <83530718+siddhigarg@users.noreply.github.com>
2026-08-26 17:06:01 +0530

    Update 2_x_1_mux.v

--- source/2_x_1_mux.v
+++ source/2_x_1_mux.v
@@ -1,3 +1,3 @@
-module 2_x_1_mux(input [31:0] input_a, input [31:0] input_b, input select_line, output [31:0] output_mux);
+module mux_2x1(input [31:0] input_a, input [31:0] input_b, input select_line, output [31:0] output_mux);
     assign output_mux = select_line ? input_b : input_a ;
 endmodule
